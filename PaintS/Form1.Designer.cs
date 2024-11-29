ac94554fcc0fd39d62a92dd1a899ba3f5d18ac70
angelina <angelina@DESKTOP-CAQK9OI>
2024-11-30 00:09:18 +0700

    Добавьте файлы проекта.

---
+++ PaintS/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace PaintS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            btnClear = new Button();
+            btnSave = new Button();
+            btnTriangle = new Button();
+            btnRectang = new Button();
+            btnEllipse = new Button();
+            btnLine = new Button();
+            btnPaint = new Button();
+            btnColor = new Button();
+            pic = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pic).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Tan;
+            panel1.Controls.Add(btnClear);
+            panel1.Controls.Add(btnSave);
+            panel1.Controls.Add(btnTriangle);
+            panel1.Controls.Add(btnRectang);
+            panel1.Controls.Add(btnEllipse);
+            panel1.Controls.Add(btnLine);
+            panel1.Controls.Add(btnPaint);
+            panel1.Controls.Add(btnColor);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(206, 722);
+            panel1.TabIndex = 0;
+            // 
+            // btnClear
+            // 
+            btnClear.Font = new Font("Comic Sans MS", 15F, FontStyle.Italic, GraphicsUnit.Point);
+            btnClear.Location = new Point(5, 642);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(94, 41);
+            btnClear.TabIndex = 8;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += BtnClear_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Font = new Font("Comic Sans MS", 15F, FontStyle.Italic, GraphicsUnit.Point);
+            btnSave.Location = new Point(105, 642);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(94, 41);
+            btnSave.TabIndex = 6;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnTriangle
+            // 
+            btnTriangle.BackgroundImage = Properties.Resources._1697703098_flomaster_top_p_chernii_treugolnik_risunok_vkontakte_48;
+            btnTriangle.BackgroundImageLayout = ImageLayout.Zoom;
+            btnTriangle.Location = new Point(44, 517);
+            btnTriangle.Name = "btnTriangle";
+            btnTriangle.Size = new Size(98, 79);
+            btnTriangle.TabIndex = 7;
+            btnTriangle.UseVisualStyleBackColor = true;
+            btnTriangle.Click += btnTriangle_Click;
+            // 
+            // btnRectang
+            // 
+            btnRectang.BackgroundImage = Properties.Resources._76c79aa1347c4c35c6b736aa8cb69067;
+            btnRectang.BackgroundImageLayout = ImageLayout.Zoom;
+            btnRectang.Location = new Point(44, 415);
+            btnRectang.Name = "btnRectang";
+            btnRectang.Size = new Size(98, 79);
+            btnRectang.TabIndex = 5;
+            btnRectang.UseVisualStyleBackColor = true;
+            btnRectang.Click += btnRectang_Click;
+            // 
+            // btnEllipse
+            // 
+            btnEllipse.BackgroundImage = Properties.Resources._1681840505_kartinki_pibig_info_p_kartinka_belii_krug_arti_vkontakte_12;
+            btnEllipse.BackgroundImageLayout = ImageLayout.Zoom;
+            btnEllipse.Location = new Point(44, 317);
+            btnEllipse.Name = "btnEllipse";
+            btnEllipse.Size = new Size(98, 79);
+            btnEllipse.TabIndex = 4;
+            btnEllipse.UseVisualStyleBackColor = true;
+            btnEllipse.Click += btnEllipse_Click;
+            // 
+            // btnLine
+            // 
+            btnLine.BackgroundImage = Properties.Resources._5cff802d48d74607ac929ec2751684e9;
+            btnLine.BackgroundImageLayout = ImageLayout.Zoom;
+            btnLine.Location = new Point(44, 218);
+            btnLine.Name = "btnLine";
+            btnLine.Size = new Size(98, 79);
+            btnLine.TabIndex = 3;
+            btnLine.UseVisualStyleBackColor = true;
+            btnLine.Click += btnLine_Click;
+            // 
+            // btnPaint
+            // 
+            btnPaint.BackgroundImage = Properties.Resources.pngtree_blue_paint_white_paint_bucket_water_droplet_paint_paint_decoration_png_image_407650;
+            btnPaint.BackgroundImageLayout = ImageLayout.Zoom;
+            btnPaint.Location = new Point(44, 120);
+            btnPaint.Name = "btnPaint";
+            btnPaint.Size = new Size(98, 79);
+            btnPaint.TabIndex = 2;
+            btnPaint.UseVisualStyleBackColor = true;
+            btnPaint.Click += btnPaint_Click;
+            // 
+            // btnColor
+            // 
+            btnColor.BackgroundImage = Properties.Resources.e85fb1190dbc6bcbc52084bdc2bca106;
+            btnColor.BackgroundImageLayout = ImageLayout.Zoom;
+            btnColor.Location = new Point(44, 21);
+            btnColor.Name = "btnColor";
+            btnColor.Size = new Size(98, 79);
+            btnColor.TabIndex = 1;
+            btnColor.UseVisualStyleBackColor = true;
+            btnColor.Click += btnColor_Click;
+            // 
+            // pic
+            // 
+            pic.Dock = DockStyle.Fill;
+            pic.Location = new Point(206, 0);
+            pic.Name = "pic";
+            pic.Size = new Size(667, 722);
+            pic.TabIndex = 0;
+            pic.TabStop = false;
+            pic.MouseClick += pic_MouseClick;
+            pic.MouseDown += pic_MouseDown;
+            pic.MouseMove += pic_MouseMove;
+            pic.MouseUp += pic_MouseUp;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(873, 722);
+            Controls.Add(pic);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pic).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button btnSave;
+        private Button btnTriangle;
+        private Button btnRectang;
+        private Button btnEllipse;
+        private Button btnLine;
+        private Button btnPaint;
+        private Button btnColor;
+        private PictureBox pic;
+        private Button btnClear;
+    }
+}
---
+++ PaintS/Form1.cs
@@ -0,0 +1,228 @@
+using System.Drawing.Imaging;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace PaintS
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bm;
+        Graphics g;
+        bool paint = false;
+        Point px, py;
+        Pen p = new Pen(Color.Black, 2);
+        Pen Eraser = new Pen(Color.White, 10);
+        int index;
+        int x, y, sx, sy, cx, cy;
+        
+
+        Color New_Color;
+        ColorDialog cd = new ColorDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+            bm = new Bitmap(pic.Width, pic.Height);
+            g = Graphics.FromImage(bm);
+            g.Clear(Color.White);
+            pic.Image = bm;
+           
+        }
+
+
+        private void pic_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true;
+            py = e.Location;
+
+            cx = e.X;
+            cy = e.Y;
+           
+        }
+
+        private void pic_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (paint)
+            {
+                if (index == 1)
+                {
+                    px = e.Location;
+                    g.DrawLine(p, px, py);
+                    py = px;
+                }
+            }
+            pic.Refresh();
+
+            x = e.X;
+            y = e.Y;
+            sx = e.X - cx;
+            sy = e.Y - cy;
+        }
+
+        private void pic_MouseUp(object sender, MouseEventArgs e)
+        {
+            paint = false;
+
+            sx = x - cx;
+            sy = y - cy;
+            if (index == 3)
+            {
+                g.DrawEllipse(p, cx, cy, sx, sy);
+            }
+            if (index == 4)
+            {
+                g.DrawRectangle(p, cx, cy, sx, sy);
+            }
+            if (index == 5)
+            {
+                g.DrawLine(p, cx, cy, x, y);
+            }
+            if (index == 7)
+            {
+                Point[] points = new Point[]
+                {
+            new Point(cx, cy),   // Вершина A
+            new Point(x, y),     // Вершина B
+            new Point(cx, y)     // Вершина C
+                };
+                g.DrawPolygon(p, points);
+            }
+        }
+
+
+        private void btnEllipse_Click(object sender, EventArgs e)
+        {
+            index = 3;
+        }
+        private void btnTriangle_Click(object sender, EventArgs e)
+        {
+            index = 7;
+        }
+
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            index = 5;
+        }
+        private void pic_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            if (paint)
+            {
+                if (index == 3)
+                {
+                    g.DrawEllipse(p, cx, cy, sx, sy);
+                }
+                if (index == 4)
+                {
+                    g.DrawRectangle(p, cx, cy, sx, sy);
+                }
+                if (index == 5)
+                {
+                    g.DrawLine(p, cx, cy, x, y);
+                    if (index == 7)
+                    {
+                        Point[] points = new Point[]
+                        {
+                        new Point(cx, cy),   // Вершина A
+                        new Point(x, y),     // Вершина B
+                        new Point(cx, y)     // Вершина C
+                        };
+                        g.DrawPolygon(p, points);
+                    }
+                }
+               
+
+            }
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            pic.Image = bm;
+            index = 0;
+        }
+        private void btnRectang_Click(object sender, EventArgs e)
+        {
+            index = 4;
+        }
+
+        private void btnPaint_Click(object sender, EventArgs e)
+        {
+            index = 6;
+        }
+
+        static Point set_Point(PictureBox pb, Point pt)
+        {
+            float px = 1f * pb.Width / pb.Width;
+            float py = 1f * pb.Height / pb.Height;
+            return new Point((int)(pt.X * px), (int)(pt.Y * py));
+        }
+        private void Validate(Bitmap bm, Stack<Point> sp, int x, int y, Color Old_Color, Color New_Color)
+        {
+            Color cx = bm.GetPixel(x, y);
+            if (cx == Old_Color)
+            {
+                sp.Push(new Point(x, y));
+                bm.SetPixel(x, y, New_Color);
+            }
+        }
+
+        public void Fill(Bitmap bm, int x, int y, Color New_Clr)
+        {
+            Color Old_Color = bm.GetPixel(x, y);
+            Stack<Point> pixel = new Stack<Point>();
+            pixel.Push(new Point(x, y));
+            bm.SetPixel(x, y, New_Clr);
+            if (Old_Color == New_Clr) { return; }
+
+            while (pixel.Count > 0)
+            {
+                Point pt = (Point)pixel.Pop();
+                if (pt.X > 0 && pt.Y > 0 && pt.X < bm.Width - 1 && pt.Y < bm.Height - 1)
+                {
+                    Validate(bm, pixel, pt.X - 1, pt.Y, Old_Color, New_Clr);
+                    Validate(bm, pixel, pt.X, pt.Y - 1, Old_Color, New_Clr);
+                    Validate(bm, pixel, pt.X + 1, pt.Y, Old_Color, New_Clr);
+                    Validate(bm, pixel, pt.X, pt.Y + 1, Old_Color, New_Clr);
+                }
+            }
+        }
+
+        private void pic_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (index == 6)
+            {
+                Point point = set_Point(pic, e.Location);
+                Fill(bm, point.X, point.Y, New_Color);
+            }
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            cd.ShowDialog();
+            New_Color = cd.Color;
+            pic.BackColor = cd.Color;
+            p.Color = cd.Color;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            sfd.Filter = "Image(*.jpg)|*.jpg|(*.*)|*.*";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap btm = bm.Clone(new Rectangle(0, 0, pic.Width, pic.Height), bm.PixelFormat);
+                btm.Save(sfd.FileName, ImageFormat.Jpeg);
+            }
+        }
+    }
+}
